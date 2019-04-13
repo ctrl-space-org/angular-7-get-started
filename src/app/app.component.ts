fa7b39542ff86f79fe66ae81921ce43a132013bf
Alexandre Rocha <alexandrececchin@gmail.com>
2019-04-13 15:43:46 -0300

    Adding style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue
+  //   }
+  // `]
+  // You can use styleUrls to point to a list of css files, or write your css in this file 
+  // using the styles
 })
 export class AppComponent {
   name = 'angular-get-started';
