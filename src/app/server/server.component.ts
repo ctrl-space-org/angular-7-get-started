2dff5d8299f75a440edd6f241c8fd32d1fa3dd00
Alexandre Rocha <alexandrececchin@gmail.com>
2019-04-13 21:10:52 -0300

    Test selectors approach

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
+    // selector: '[app-servers]',
+    // selector: '.app-servers',
     templateUrl: './server.component.html',
 })
 export class ServerComponent {
-    
+
 }
